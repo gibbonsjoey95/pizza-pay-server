ebced71a2585c75a0e8e00b3a0a39e613aa7e8c0
Joey Gibbons <gibbonsjoey95@gmail.com>
2023-03-09 09:41:34 -0600

    set up basic server functionality

--- app.js
+++ app.js
@@ -1,1 +1,29 @@
-console.log('Here');
+require('dotenv').config();
+
+const express = require('express');
+const app = express();
+
+const notFoundMiddleware = require('./middleware/not-found');
+const errorMiddleware = require('./middleware/error-handler');
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('this will be my API');
+});
+
+app.use(notFoundMiddleware);
+app.use(errorMiddleware);
+
+const port = process.env.PORT || 4000;
+
+const start = async () => {
+  try {
+    // connectDB
+    app.listen(port, console.log(`Server is listening on port ${port}...`));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
