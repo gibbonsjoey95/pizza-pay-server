40ea47173e91246ecf1cbbb7dcb0b2c0a7bfdb0e
Joey Gibbons <gibbonsjoey95@gmail.com>
2023-03-14 21:32:40 -0500

    set up a basic server

--- app.js
+++ app.js
@@ -1,16 +1,18 @@
 require('dotenv').config();
+require('express-async-errors');
 
 const express = require('express');
 const app = express();
+
+const connectDB = require('./db/connect');
+const productsRouter = require('./routes/products');
 
 const notFoundMiddleware = require('./middleware/not-found');
 const errorMiddleware = require('./middleware/error-handler');
 
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('this will be my API');
-});
+app.use('/', productsRouter);
 
 app.use(notFoundMiddleware);
 app.use(errorMiddleware);
@@ -20,6 +22,7 @@ const port = process.env.PORT || 4000;
 const start = async () => {
   try {
     // connectDB
+    await connectDB(process.env.MONGO_URI);
     app.listen(port, console.log(`Server is listening on port ${port}...`));
   } catch (error) {
     console.log(error);
---
+++ controllers/products.js
@@ -0,0 +1,5 @@
+const getAllProducts = async (req, res) => {
+  res.json({ msg: 'This will be my API here' });
+};
+
+module.exports = { getAllProducts };
